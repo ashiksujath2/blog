f0daa04187ed9a10bd564daa48f1f262e0f90b8d
Ashik Sujath <ashiksujath2@gmail.com>
2017-04-13 08:41:50 +0530

    placeholder template

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from blog.api.views import ArticleListView, RandomArticleListView, ArticleDetailView
+from blog.views import HomeView
 
 urlpatterns = [
     url(r'^api/articles$', ArticleListView.as_view()),
     url(r'^api/random/articles$', RandomArticleListView.as_view()),
     url(r'^api/article/(?P<pk>\d+)$', ArticleDetailView.as_view()),
+    url(r'^$', HomeView.as_view())
 ]
